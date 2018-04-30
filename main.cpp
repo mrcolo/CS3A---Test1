0f89dfdab9b62cf885c0185df2a567fef7988379
Francesco Colonnese <colo28197@gmail.com>
2018-04-30 10:29:26 -0700

    introduced function pointers

--- main.cpp
+++ main.cpp
@@ -5,20 +5,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sstream>
+
 using namespace std;
 
+typedef void (*fptr)(string, polynomial [], string);
+
 string getInput(const string prompt, string& line);
-void evalCommand(string line, polynomial polys [26]);
+void evalCommand(string line, polynomial polys [26],fptr f[7]);
 bool exitCondition(string line);
 void cleanInput(string& line);
 bool fileExists(const string& filename);
 int getDerivation(string s );
+
+void LET(string line, polynomial polys [26], string ALPHABET);
+void EVAL(string line, polynomial polys [26], string ALPHABET);
+void PRINT(string line, polynomial polys [26], string ALPHABET);
+void SAVE(string line, polynomial polys [26], string ALPHABET);
+void LOAD(string line, polynomial polys [26], string ALPHABET);
+void HELP(string line, polynomial polys [26], string ALPHABET);
+void EXIT(string line, polynomial polys [26], string ALPHABET);
 
 int main(int argc, char* argv[]){
 
     const string WELCOMEMSG = "Welcome to Expression Calculator. If you don't know what to do, type HELP.\n",
                  INPUTPROMPT = "INPUT: ";
 
+     fptr commands[7] = {&LET,&EVAL,&PRINT,&SAVE,&LOAD,&HELP,&EXIT};
     //TODO Create Functions for each case.
     switch (argc){
         case 2:
@@ -37,11 +49,11 @@ int main(int argc, char* argv[]){
     //Basic Controller
     do{
         try {
-            evalCommand(getInput(INPUTPROMPT, command), polys);
+            evalCommand(getInput(INPUTPROMPT, command), polys, commands);
 
         }
         catch (string& e){
-            cout << "ERROR:"<<endl<<e<<endl;
+            cout << "ERROR:"<<endl<<e<<endl<<endl;
         }
 
     }while(exitCondition(command));
@@ -62,20 +74,10 @@ string getInput(const string prompt, string& line){
     return line;
 }
 
-void evalCommand(string line, polynomial polys[26]){
-    const string ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-                 INSTRUCTIONS =
-                    "POSSIBLE INSTRUCTIONS: \n\n"
-                    "- LET  => Stores algebraic expression as capital letter A-Z\n\n"
-                    "\texample: LET F = 2X + 4\n\n"
-                    "- EVAL => evaluates pre-stored algebraic expression at a given value\n\n"
-                    "\texample: EVAL F(1/4)\n\n"
-                    "- PRINT => prints pre-stored algebraic expression\n\n"
-                    "\texample: PRINT F\n\n"
-                    "- SAVE => saves in the following file 26 pre-stored algebraic expression\n\n"
-                    "\texample: SAVE file.exp\n\n"
-                    "- LOAD => loads from the following file 26 pre-stored algebraic expression\n\n"
-                    "\texample: LOAD file.exp\n\n";
+void evalCommand(string line, polynomial polys[26], fptr f[7]){
+
+    const string ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
 
     string command(line.substr(0,line.find(' ')));
 
@@ -83,143 +85,29 @@ void evalCommand(string line, polynomial polys[26]){
     for(int i = 0; i < command.length(); i++ )
         command[i] = toupper(command[i]);
 
+    //f[choice](v);
+
     //TODO use function pointers to execute rather than if statements
     if(command == "LET"){
-        string exception;
-        size_t pos;
-
-
-        if((pos = line.find('=')) != string::npos){
-            char current_exp = toupper(line[pos-2]);
-
-            polynomial p(line.substr(pos+2, string::npos));
-
-            cout<<"Generated "<<current_exp<< " = "<<p<<endl<<endl;
-
-            polys[ALPHABET.find(current_exp)] = p;
-            cout<<polys[ALPHABET.find(current_exp)]<<endl;
-        }
-        else{
-            exception = "LET not valid. Function not provided.";
-            throw exception;
-        }
+        f[0](line,polys,ALPHABET);
     }
     else if(command == "EVAL"){
-
-        string current_eval = line.substr(line.find("EVAL")+6,string::npos);
-
-        current_eval[0] = toupper(current_eval[0]);
-
-        polys[ALPHABET.find(current_eval[0])].evaluate(atoi(&current_eval[2]));
-
+        f[1](line,polys,ALPHABET);
     }
     else if(command == "PRINT"){
-        string current_print = line.substr(line.find("PRINT")+7,string::npos);
-
-        if(current_print.length() == 1){
-            current_print = toupper(current_print[0]);
-            cout<<current_print<<" = "<<polys[ALPHABET.find(current_print)]<<endl;
-        }
-        else{
-            int pos;
-            if((pos = current_print.find("=")) != string::npos){
-
-                string editFunction = current_print.substr(pos-2,pos-1);
-                editFunction[0] = toupper(editFunction[0]);
-                string instruction = current_print.substr(pos+2,string::npos);
-
-                char exp1,exp2,op;
-                if(instruction.find('\'') == string::npos){
-                    exp1 = toupper(instruction[0]);
-                    op = instruction [2];
-                    exp2 = toupper(instruction[4]);
-
-                    switch(op){
-                        case '+':
-                            cout<<"RESULT: "<< polys[ALPHABET.find(exp1)] + polys[ALPHABET.find(exp2)]<<endl;
-                            polys[ALPHABET.find(editFunction[0])] = polys[ALPHABET.find(exp1)] + polys[ALPHABET.find(exp2)];
-                            break;
-                        case '-':
-                            cout<<"RESULT: "<< polys[ALPHABET.find(exp1)] - polys[ALPHABET.find(exp2)]<<endl;
-                            polys[ALPHABET.find(editFunction[0])] = polys[ALPHABET.find(exp1)] - polys[ALPHABET.find(exp2)];
-                            break;
-                        case '*':
-                            cout<<"RESULT: "<< polys[ALPHABET.find(exp1)] * polys[ALPHABET.find(exp2)]<<endl;
-                            polys[ALPHABET.find(editFunction[0])] = polys[ALPHABET.find(exp1)] * polys[ALPHABET.find(exp2)];
-                            break;
-
-                        default:
-                            string exception = "BAD_INPUT";
-                            throw exception;
-                    }
-                }
-                else{
-                    int editFunctionIndex = ALPHABET.find(editFunction[0]);
-
-                    char receiver = toupper(instruction[0]);
-
-                    polys[editFunctionIndex] = polys[ALPHABET.find(receiver)].returnDerivative(getDerivation(instruction));
-
-                    cout<<editFunction<<" = "<<polys[ALPHABET.find(receiver)].returnDerivative(getDerivation(instruction))<<endl;
-                }
-
-
-
-
-
-            }
-
-
-
-
-        }
-
-
+        f[2](line,polys,ALPHABET);
     }
     else if(command == "SAVE") {
-        string outfile_name(line.substr(line.find("SAVE")+6, string::npos));
-        if(fileExists(outfile_name)) {
-            string filename_error = "FILENAME_ALREADY_EXISTS";
-            throw filename_error;
-        }
-        else {
-            ofstream outfile;
-            outfile.open(outfile_name);
-            for(size_t i = 0; i < 26; ++i) {
-                outfile<<polys[i]<<endl;
-            }
-            outfile.close();
-        }
-        //TODO add a few couts that explain what the program is doing
+        f[3](line,polys,ALPHABET);
     }
     else if(command == "LOAD") {
-        string infile_name(line.substr(line.find("LOAD")+6, string::npos));
-
-        if(fileExists(infile_name)) {
-            string expression;
-            int lineNumber = 0;
-            ifstream infile;
-            infile.open(infile_name);
-            while(getline(infile, expression)) {
-                polynomial temp_poly(expression);
-
-                polys[lineNumber] = temp_poly;
-                lineNumber++;
-            }
-            infile.close();
-        }
-        //TODO add a few couts that explain what the program is doing
-        else {
-            string file_error = "FILE_DOESNT_EXIST";
-            throw file_error;
-        }
+        f[4](line,polys,ALPHABET);
     }
     else if(command == "EXIT" || command == ""){
-        cout<<"Exiting Expression Calculator..."<<endl;
-        return;
+        f[6](line,polys,ALPHABET);
     }
     else if(command == "HELP"){
-        cout<<INSTRUCTIONS<<endl;
+        f[5](line,polys,ALPHABET);
     }
     else
     {
@@ -254,7 +142,7 @@ bool fileExists(const string &filename) {
 }
 
 int getDerivation(string s ) {
-
+    //get the number of the derivative we are looking for.
     int counter = 0;
 
     for (int i = 0; i < s.length(); i++) {
@@ -263,3 +151,145 @@ int getDerivation(string s ) {
     }
     return counter;
 }
+
+void LET(string line, polynomial polys [26], string ALPHABET){
+    string exception;
+    size_t pos;
+
+
+    if((pos = line.find('=')) != string::npos){
+        char current_exp = toupper(line[pos-2]);
+
+        polynomial p(line.substr(pos+2, string::npos));
+
+        cout<<"Generated "<<current_exp<< " = "<<p<<endl<<endl;
+
+        polys[ALPHABET.find(current_exp)] = p;
+        cout<<polys[ALPHABET.find(current_exp)]<<endl;
+    }
+    else{
+        exception = "LET not valid. Function not provided.";
+        throw exception;
+    }
+}
+void EVAL(string line, polynomial polys [26], string ALPHABET){
+    string current_eval = line.substr(line.find("EVAL")+6,string::npos);
+
+    current_eval[0] = toupper(current_eval[0]);
+
+    polys[ALPHABET.find(current_eval[0])].evaluate(atoi(&current_eval[2]));
+
+}
+void PRINT(string line, polynomial polys [26], string ALPHABET){
+    string current_print = line.substr(line.find("PRINT")+7,string::npos);
+
+    if(current_print.length() == 1){
+        current_print = toupper(current_print[0]);
+        cout<<current_print<<" = "<<polys[ALPHABET.find(current_print)]<<endl;
+    }
+    else{
+        int pos;
+        if((pos = current_print.find("=")) != string::npos){
+
+            string editFunction = current_print.substr(pos-2,pos-1);
+            editFunction[0] = toupper(editFunction[0]);
+            string instruction = current_print.substr(pos+2,string::npos);
+
+            char exp1,exp2,op;
+            if(instruction.find('\'') == string::npos){
+                exp1 = toupper(instruction[0]);
+                op = instruction [2];
+                exp2 = toupper(instruction[4]);
+
+                switch(op){
+                    case '+':
+                        cout<<"RESULT: "<< polys[ALPHABET.find(exp1)] + polys[ALPHABET.find(exp2)]<<endl;
+                        polys[ALPHABET.find(editFunction[0])] = polys[ALPHABET.find(exp1)] + polys[ALPHABET.find(exp2)];
+                        break;
+                    case '-':
+                        cout<<"RESULT: "<< polys[ALPHABET.find(exp1)] - polys[ALPHABET.find(exp2)]<<endl;
+                        polys[ALPHABET.find(editFunction[0])] = polys[ALPHABET.find(exp1)] - polys[ALPHABET.find(exp2)];
+                        break;
+                    case '*':
+                        cout<<"RESULT: "<< polys[ALPHABET.find(exp1)] * polys[ALPHABET.find(exp2)]<<endl;
+                        polys[ALPHABET.find(editFunction[0])] = polys[ALPHABET.find(exp1)] * polys[ALPHABET.find(exp2)];
+                        break;
+
+                    default:
+                        string exception = "BAD_INPUT";
+                        throw exception;
+                }
+            }
+            else{
+                int editFunctionIndex = ALPHABET.find(editFunction[0]);
+
+                char receiver = toupper(instruction[0]);
+
+                polys[editFunctionIndex] = polys[ALPHABET.find(receiver)].returnDerivative(getDerivation(instruction));
+
+                cout<<editFunction<<" = "<<polys[ALPHABET.find(receiver)].returnDerivative(getDerivation(instruction))<<endl;
+            }
+        }
+    }
+}
+void SAVE(string line, polynomial polys [26], string ALPHABET){
+    string outfile_name(line.substr(line.find("SAVE")+6, string::npos));
+
+    if(fileExists(outfile_name)) {
+        string filename_error = "FILENAME_ALREADY_EXISTS";
+        throw filename_error;
+    }
+    else {
+        ofstream outfile;
+        outfile.open(outfile_name);
+        for(size_t i = 0; i < 26; ++i) {
+            outfile<<polys[i]<<endl;
+        }
+        outfile.close();
+    }
+
+}
+void LOAD(string line, polynomial polys [26], string ALPHABET){
+
+    string infile_name(line.substr(line.find("LOAD")+6, string::npos));
+
+    if(fileExists(infile_name)) {
+        string expression;
+        int lineNumber = 0;
+        ifstream infile;
+        infile.open(infile_name);
+        while(getline(infile, expression)) {
+            polynomial temp_poly(expression);
+
+            polys[lineNumber] = temp_poly;
+            lineNumber++;
+        }
+        infile.close();
+    }
+    else {
+        string file_error = "FILE_DOESNT_EXIST";
+        throw file_error;
+    }
+
+}
+void HELP(string line, polynomial polys [26], string ALPHABET){
+    const string INSTRUCTIONS =
+            "POSSIBLE INSTRUCTIONS: \n\n"
+                    "- LET  => Stores algebraic expression as capital letter A-Z\n\n"
+                    "\texample: LET F = 2X + 4\n\n"
+                    "- EVAL => evaluates pre-stored algebraic expression at a given value\n\n"
+                    "\texample: EVAL F(1/4)\n\n"
+                    "- PRINT => prints pre-stored algebraic expression\n\n"
+                    "\texample: PRINT F\n\n"
+                    "- SAVE => saves in the following file 26 pre-stored algebraic expression\n\n"
+                    "\texample: SAVE file.exp\n\n"
+                    "- LOAD => loads from the following file 26 pre-stored algebraic expression\n\n"
+                    "\texample: LOAD file.exp\n\n";
+
+    cout<<INSTRUCTIONS<<endl;
+}
+void EXIT(string line, polynomial polys [26], string ALPHABET){
+    cout<<"Exiting Expression Calculator..."<<endl;
+    return;
+}
+
